a34e1b1df6ebc7ba9d7cfc224cb5990f91f64fd0
TrapShooter <thomassoto7@gmail.com>
2026-05-12 17:53:37 -0400

    Cleanup

--- app/manifest.ts
+++ app/manifest.ts
@@ -11,11 +11,6 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: '#000000',
     icons: [
       {
-        src: '/favicon.ico',
-        sizes: 'any',
-        type: 'image/x-icon',
-      },
-      {
         src: '/icon.png',
         sizes: '192x192',
         type: 'image/png',
